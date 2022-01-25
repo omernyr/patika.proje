8f81a65f51c5a3581a550713bb5f2acd200ac5c3
omernyr <89137487+omernyr@users.noreply.github.com>
2022-01-25 17:01:03 +0300

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[86]:
+
+
+list = [[1, 2], [3, 4], [5, 6, 7]]
+
+
+
+# In[87]:
+
+
+list.reverse()
+list[0].reverse()
+list[1].reverse()
+list[2].reverse()
+
+
+# In[82]:
+
+
+list
+
